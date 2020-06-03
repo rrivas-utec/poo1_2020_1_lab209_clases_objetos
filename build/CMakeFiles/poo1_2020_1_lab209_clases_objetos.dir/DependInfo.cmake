de67a1c2896059bd1546039743714df5dc284ccf
Ruben Rivas <rrivas@utec.edu.pe>
2020-06-03 16:17:31 +0000

    .replit was added

---
+++ build/CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/runner/poo120201lab209clasesobjetos-1/main.cpp" "/home/runner/poo120201lab209clasesobjetos-1/build/CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/main.cpp.o"
+  "/home/runner/poo120201lab209clasesobjetos-1/triangulo.cpp" "/home/runner/poo120201lab209clasesobjetos-1/build/CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/triangulo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/main.cpp.o"
+  "CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/triangulo.cpp.o"
+  "poo1_2020_1_lab209_clases_objetos.pdb"
+  "poo1_2020_1_lab209_clases_objetos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poo1_2020_1_lab209_clases_objetos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
